eef46cd0af1e49b870e62d7a55ae068d61cda8ad
Catherine <katya.simonova.1997@mail.ru>
2023-02-21 16:25:53 +0300

    Добавьте файлы проекта.

---
+++ Exercise5.5_2/Program_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5._5_2
+{
+	internal class Program_2
+	{
+		static void Main(string[] args)
+		// Перестоновка слов в предложении
+		{
+			WriteLine("Введите предложение:");
+			string text = ReadLine();
+			string[] sep = Revers(text);
+
+			for(int i = 0; i < sep.Length; i++)
+			{
+				Write($"{sep[i]} ");
+			}
+			ReadLine();
+		}
+
+		/// <summary>
+		/// Разбивание строки на подстроки
+		/// </summary>
+		 static string[] Separ(string text)
+		{
+			string[] sep = text.Split(' ', ',', '.');
+
+			//foreach (var se in sep)
+			//{
+			//	WriteLine($"{se}");
+
+			//}
+			return sep;
+		}
+
+		/// <summary>
+		/// Меняем слова местами
+		/// </summary>
+		 static string[] Revers(string text)
+		{
+			string[] sep = Separ(text);
+			Array.Reverse(sep);
+			return sep;
+		}
+	}
+}
---
+++ Exercise_5.5/Program_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static  System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5._5
+{
+	class Program_1
+	{
+	/// <summary>
+	/// Запрос на ввод предложения
+	/// </summary>
+		static void Deline()
+		{
+				WriteLine("Введите предложение:");
+		}
+	/// <summary>
+	/// Разбивание строки на подстроки
+	/// </summary>
+		static void Separ()
+		{
+			string s = ReadLine();
+			string[] sep = s.Split(' ');
+
+			foreach (var se in sep)
+			{
+				WriteLine($"{se}");
+			}
+		
+		}
+
+	
+		static void Main(string[] args)
+		// Задание №_1 "Раздеение строки на слова"
+		{
+			Deline();
+			Separ();
+		}
+	}
+}
